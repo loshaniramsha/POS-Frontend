160ff9528e333968d0f5f88103e337eb454365d0
loshaniramsha <loshanikottagoda@gmail.com>
2024-08-28 19:37:35 +0530

    changes customer,orderdetails mode classes

--- pos-system/model/OrderDetailsModel.js
+++ pos-system/model/OrderDetailsModel.js
@@ -1,12 +1,12 @@
-export class OrderDetailsModel{
-    constructor(orderId,itemId,unitPrice,qty,total) {
+
+export class OrderDetailsModel {
+    constructor(orderId, itemCode, unitPrice, qty, total) {
         this._orderId = orderId;
-        this._itemId = itemId;
+        this._itemCode = itemCode;
         this._unitPrice = unitPrice;
         this._qty = qty;
         this._total = total;
     }
-
 
     get orderId() {
         return this._orderId;
@@ -16,12 +16,12 @@ export class OrderDetailsModel{
         this._orderId = value;
     }
 
-    get itemId() {
-        return this._itemId;
+    get itemCode() { // Ensure consistency here
+        return this._itemCode;
     }
 
-    set itemId(value) {
-        this._itemId = value;
+    set itemCode(value) { // Ensure consistency here
+        this._itemCode = value;
     }
 
     get unitPrice() {
