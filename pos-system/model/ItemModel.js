c516b853840cecb37002ae47077d20e856b4c695
loshaniramsha <loshanikottagoda@gmail.com>
2024-08-31 08:41:19 +0530

    changes itemModel

--- pos-system/model/ItemModel.js
+++ pos-system/model/ItemModel.js
@@ -1,32 +1,10 @@
 export default class ItemModel{
     constructor(itemCode, name, price, qty) {
-       this._itemCode = itemCode;
-        this._name = name;
-        this._price = price;
-        this._qty = qty;
+       this.itemId = itemCode;
+        this.itemName = name;
+        this.itemPrice = price;
+        this.itemQty = qty;
     }
-    get itemCode() {
-        return this._itemCode;
-    }
-    get name() {
-        return this._name;
-    }
-    get price() {
-        return this._price;
-    }
-    get qty() {
-        return this._qty;
-    }
-    set itemCode(itemCode) {
-        this._itemCode = itemCode;
-    }
-    set name(name) {
-        this._name = name;
-    }
-    set price(price) {
-        this._price = price;
-    }
-    set qty(qty) {
-        this._qty = qty;
-    }
+
+
 }
